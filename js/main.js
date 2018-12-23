98f1ec165c46495d615b3233c3e5be9812b22c8e
Renan Oliveira Rodrigues <renan.oliveira.webdesigner@gmail.com>
2018-12-23 09:58:13 -0300

    Arquivos bases do projeto

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Checando se o documento foi carregado
+$(document).ready(function(){
+
+	$(".mostrar-curso").click(function(){
+		$("#cursos-web").slideToggle("slow");
+	});
+
+	$(".mostrar-curso").click(function(){
+		$("#cursos-java").slideToggle("slow");
+	});		
+});
